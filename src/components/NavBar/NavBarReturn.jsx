b2540bd22d4791ff2fbf16f45f53d8cfb19bfbca
Colon Digital 6 <colondigital6@iMac-de-Colon.local>
2023-10-05 15:35:24 -0300

    Fixing Return Button issue

--- src/components/NavBar/NavBarReturn.jsx
+++ src/components/NavBar/NavBarReturn.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { ChevronLeftIcon} from '@heroicons/react/24/outline';
+import { ChevronLeftIcon } from '@heroicons/react/24/outline';
 
 const NavBarReturn = () => {
     const { t } = useTranslation();
@@ -12,7 +12,7 @@ const NavBarReturn = () => {
         navigate('/');
     };
 
-    const showButton = location.pathname !== '/';
+    const showButton = !['/', '/es/', '/en/'].includes(location.pathname);
 
     return showButton ? (
         <button onClick={handleGoHome} className="flex items-center text-white ml-5 bg-black py-2 px-2 text-base bg-opacity-40 h-fit">
